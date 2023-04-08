7f5694d06fca6d4730b3a05b8050ccbd6da51e40
BoyChai <1972567225@qq.com>
2023-04-08 17:24:38 +0800

    Add DingDingBotSignCheck

--- run/gin.go
+++ run/gin.go
@@ -3,8 +3,10 @@ package run
 import (
 	"fmt"
 	"github.com/BoyChai/CoralBot/bot"
+	"github.com/BoyChai/CoralBot/config"
 	"github.com/gin-gonic/gin"
 	"io"
+	"strconv"
 )
 
 func Run(e bot.Event, port string) {
@@ -13,10 +15,21 @@ func Run(e bot.Event, port string) {
 
 	// 接收上报
 	g.POST("/", func(c *gin.Context) {
+		var err error
 		dataReader := c.Request.Body
 		bodyData, err := io.ReadAll(dataReader)
 		if err != nil {
 			fmt.Println(err)
+			err = nil
+		}
+		if e.GetType() == "DingDing" {
+			timestamp, err := strconv.ParseInt(c.Request.Header.Get("timestamp"), 10, 64)
+			if err != nil {
+				fmt.Println(err)
+			}
+			config.Timestamp = timestamp
+			config.Sign = c.Request.Header.Get("sign")
+			defer c.Request.Header.Clone()
 		}
 		e.Explain(bodyData)
 	})
